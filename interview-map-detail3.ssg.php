7620fe11ee4a562256cedea51c481f597b8fb776
jhs512 <jangka512@gmail.com>
2021-08-20 23:37:04 +0900

    작업

---
+++ interview-map-detail3.ssg.php
@@ -0,0 +1,36 @@
+<?php require_once "config.php" ?>
+<?php require_once "head.php" ?>
+<section class="section-quiz-1 flex items-center justify-center min-h-screen">
+  <div class="border-4 m-4 p-4 rounded-3xl max-w-2xl bg-color-2 overflow-hidden map-box  ">
+    <div>
+      <img class="rounded-full my-4" src="https://ifh.cc/g/3esG5I.jpg" alt="">
+      <div class="text-center  text-2xl"><span>짬뽕애 사장님</span></div>
+      <div class="my-4  text-center bg-white rounded-3xl p-4 text-1xl ">
+
+
+        <div class=" max-w-xs  max-h-xs">
+
+        </div>
+
+        <div class="mb-4">
+          <span class="text-2xl ">Q. 쓰레기 무단투기로 인해 불편을 겪은 적이 있으신가요? </span>
+        </div><span class="a">네, 쓰레기에서 악취가 나서 불편하고 미관상으로 보기 불편합니다. 그리고 쓰레기
+          주변에 벌레가 꼬여 불편합니다. 또한, 쓰레기 배출 구역이 불분명하여 무단투기 된 쓰레기가 많아 거리가 혼잡합니다.</span>
+      </div>
+
+
+    </div>
+    <div class="px-5 mt-6 relative flex">
+
+
+
+
+    </div>
+
+    <div class="mt-8 mb-4 text-center">
+      <a href="interview-map.ssg.php" class="btn btn-primary ">X</a>
+    </div>
+  </div>
+  </div>
+</section>
+<?php require_once "foot.php" ?>
